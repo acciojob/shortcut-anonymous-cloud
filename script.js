3d2bcc9e7c7651f656318ba88bd37e3a0b48c4e8
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2024-08-15 15:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function shortcut(str1, str2) {
   // your code here
-	function shortcut(str1, str2) {
+	
  
   if (!str1 || !str2) {
     return "''";
@@ -15,7 +15,7 @@ console.log(shortcut("Amnesty", "International"));
 console.log(shortcut("Hello", "world"));           
 console.log(shortcut("", "International"));        
 console.log(shortcut("Amnesty", ""));              
-}
+
 
 // Do not change the code below.
 const str1 = prompt("Enter s1:");
