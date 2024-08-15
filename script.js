9ab818f0e3d0a6dcc4a3577e0785abfb5c27cabf
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2024-08-15 15:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function shortcut(s1, s2) {
   // your code here
+	function shortcut(str1, str2) {
+ 
+  if (!str1 || !str2) {
+    return "''";
+  }
+  
+  
+  return str1[0] + str2[0];
+}
+
+
+console.log(shortcut("Amnesty", "International")); 
+console.log(shortcut("Hello", "world"));           
+console.log(shortcut("", "International"));        
+console.log(shortcut("Amnesty", ""));              
 }
 
 // Do not change the code below.
